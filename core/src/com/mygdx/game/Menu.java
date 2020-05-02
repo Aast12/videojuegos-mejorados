1691d4afe6f3bf5b306b1db6552ad062b6a7509a
JeG99 <a00824494@itesm.mx>
2020-05-02 07:39:26 -0500

    started implementing menus system

---
+++ core/src/com/mygdx/game/Menu.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author JeG99
+ */
+public class Menu implements Screen {
+
+    private VMGame game;
+    private String title;
+    private LinkedList<Button> options;
+    private boolean visible;
+    private Texture background;
+    OrthographicCamera camera;
+
+    public Menu(VMGame game, String title, LinkedList<Button> options, Texture background) {
+        this.title = title;
+        this.game = game;
+        this.options = options;
+        this.background = background;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 600);
+    }
+
+    public LinkedList<Button> getOptions() {
+        return options;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setBackground(Texture background) {
+        this.background = background;
+    }
+
+    @Override
+    public void render(float f) {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        //game.batch.begin();
+        game.batch.draw(background, 0, 0);
+        for (Button button : options) {
+            button.render(game.batch);
+        }
+        game.font.draw(game.batch, title, 400, 520);
+        //game.batch.end();
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
--- core/src/com/mygdx/game/VMGame.java
+++ core/src/com/mygdx/game/VMGame.java
@@ -1,17 +1,23 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import java.util.LinkedList;
 
-public class VMGame extends ApplicationAdapter {
+public class VMGame extends Game {
 
     private Rectangle badlogic;
+    OrthographicCamera camera;
     SpriteBatch batch;
+    BitmapFont font;
     Texture img;
     Enemy man1;
     Item item1 = null;
@@ -21,8 +27,16 @@ public class VMGame extends ApplicationAdapter {
     boolean isDashing;
     long dashTime;
 
+    // for main menu
+    private Menu mainMenu;
+    private LinkedList<Button> mainMenuOptions;
+
     @Override
+
     public void create() {
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 600);
+
         badlogic = new Rectangle();
         badlogic.x = 800 / 2 - 64 / 2;
         badlogic.y = 20;
@@ -35,6 +49,24 @@ public class VMGame extends ApplicationAdapter {
         lastHit = System.nanoTime();
         health = 100;
         acceleration = 0;
+
+        // for main menu
+        mainMenuOptions = new LinkedList<Button>();
+        Texture mainMenuMaterial = new Texture("material1.png");
+        Button start = new Button(60, 450, 128, 32, "START", mainMenuMaterial);
+        Button load = new Button(60, 485, 128, 32, "LOAD", mainMenuMaterial);
+        Button options = new Button(60, 520, 128, 32, "OPTIONS", mainMenuMaterial);
+        Button exit = new Button(60, 555, 128, 32, "EXIT", mainMenuMaterial);
+        mainMenuOptions.add(start);
+        mainMenuOptions.add(load);
+        mainMenuOptions.add(options);
+        mainMenuOptions.add(exit);
+        Texture background = new Texture("main_menu_background.png");
+        mainMenu = new Menu(this, "THE GAME", mainMenuOptions, background);
+        mainMenu.setVisible(true);
+
+        font = new BitmapFont();
+        //this.setScreen(new Menu(this, "THE GAME", mainMenuOptions, background));
     }
 
     @Override
@@ -43,14 +75,21 @@ public class VMGame extends ApplicationAdapter {
             isDashing = false;
             acceleration = 0;
         }
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 1, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-	    man1.tick();
+        super.render();
+        man1.tick();
         batch.draw(img, badlogic.x, badlogic.y);
-	    batch.draw(man1.img, man1.x, man1.y);
-	    if(item1 != null) {
+        batch.draw(man1.img, man1.x, man1.y);
+        if (item1 != null) {
             batch.draw(item1.img, item1.x, item1.y);
+        }
+        for (Button button : mainMenu.getOptions()) {
+            if (!button.getBox().contains(Gdx.input.getX(), Gdx.input.getY())) {
+                System.out.println(button.getMessage());
+                mainMenu.render(Gdx.graphics.getDeltaTime());
+            }
         }
         batch.end();
 
@@ -70,12 +109,10 @@ public class VMGame extends ApplicationAdapter {
 
         }
         if (Gdx.input.isKeyPressed(Input.Keys.E)) {// RECOGER
-            if(item1 != null) {
-                if (badlogic.overlaps(item1.hitbox))
-                {
+            if (item1 != null) {
+                if (badlogic.overlaps(item1.hitbox)) {
                     double now = System.nanoTime();
-                    if (now - lastHit > 1000000000)
-                    {
+                    if (now - lastHit > 1000000000) {
                         item1 = null;
                         lastHit = now;
                     }
@@ -101,5 +138,6 @@ public class VMGame extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
         img.dispose();
+        font.dispose();
     }
 }
