e306cb04a88ca7e4d5f167c6ab2c7469a83a28b6
Alam Sanchez <asancheztorres1905@gmail.com>
2020-06-02 09:06:18 -0500

    Add tilesv3

---
+++ core/assets/tilesv3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="tilesv3" tilewidth="64" tileheight="64" tilecount="30" columns="5">
+ <image source="tilesv3.png" width="320" height="384"/>
+</tileset>
